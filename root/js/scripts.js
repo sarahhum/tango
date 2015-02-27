717605409d83d835cd6ecafcc96dde59e736c954
Sarah Hum <hello@sarahhum.com>
2015-02-26 19:13:51 -0500

    Planner page: calendar stylingand card sorting

--- root/js/scripts.js
+++ root/js/scripts.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-  $(function(){
-    $('#mix-cards').mixItUp();
+
+  $('#mix-cards').mixItUp({
+    animation: {
+      enable: false   
+    },
+    callbacks: {
+      onMixLoad: function(){
+        $(this).mixItUp('setOptions', {
+          animation: {
+            enable: true  
+          },
+        });
+      }
+    }
+  }); 
+
+  $('#planner-events, #planner-countdowns').mixItUp({
+    animation: {
+      enable: false   
+    }
   });
+
 });
