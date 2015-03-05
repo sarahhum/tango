583bb461e273adb5b4f278bb49b99d7b29cf6dcd
Sarah Hum <hello@sarahhum.com>
2015-03-05 13:21:36 -0500

    Show the correct event labels in planner

--- root/js/scripts.js
+++ root/js/scripts.js
@@ -4,37 +4,28 @@ $(document).ready(function(){
     $('.flexslider').flexslider();
   });
   
-  $('#mix-cards').mixItUp({
-    animation: {
-      enable: false   
-    }
-    // },
-    // callbacks: {
-    //   onMixLoad: function(){
-    //     $(this).mixItUp('setOptions', {
-    //       animation: {
-    //         enable: true  
-    //       },
-    //     });
-    //   }
-    // }
-  }); 
 
-  $('#planner-events, #planner-countdowns').mixItUp({
+  $('#mix-cards, #planner-events, #planner-countdowns').mixItUp({
     animation: {
       enable: false   
     }
   });
 
+  // Show/hide item details
   $('.card').on('click', function () {
     console.log('click');
     $('.item-detail').show();
   });
-
   $('.black-overlay').on('click', function () {
     $('.item-detail').hide();
   });
 
+  // Show the right even labels in planner
+  $('.sidebar-card').on('click', function () {
+      console.log($(this).attr("id"));
+      $('#planner-countdowns li').removeClass('active');
+      $('#planner-countdowns li.' + $(this).attr("id")).addClass('active');
+  });
 
 
 });
